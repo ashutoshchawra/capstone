bdf5cb3e85d0128feb329d1dad59c1bfb645dca4
Akshat <akchawra306@gmail.com>
2023-02-21 16:57:48 +0530

    new commit

--- src/main/java/com/collebra/capstone/controller/MovieController.java
+++ src/main/java/com/collebra/capstone/controller/MovieController.java
@@ -4,18 +4,22 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.collebra.capstone.entity.Movies;
 import com.collebra.capstone.service.MovieService;
 
 @RestController
+@CrossOrigin("http://localhost:8080")
+@RequestMapping
 public class MovieController {
 
 	@Autowired
